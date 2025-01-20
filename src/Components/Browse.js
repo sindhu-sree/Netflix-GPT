eb62e296b70299f2a49bbf3c5827b75b53774ac2
sindhu-sree <sindunarayana22@gmail.com>
2025-01-20 13:21:59 +0530

    GPT Search UI and Implemented Multi-language Support feature

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -3,16 +3,17 @@ import Header from './Header'
 import useNowPlayingMovies from '../Hooks/useNowPlayingMovies'
 import MainContainer from './MainContainer'
 import SecondaryContainer from './SecondaryContainer'
+import GptSearch from './GptSearch'
+import { useSelector } from 'react-redux'
 
 const Browse = () => {
-
+  const isGptToggle = useSelector(store=>store.gpt?.showGptSearch)
   useNowPlayingMovies()
 
   return (
     <div>
       <Header />
-      <MainContainer/>
-      <SecondaryContainer/>
+      {isGptToggle ? <GptSearch/>:<><MainContainer /> <SecondaryContainer/></>}
       {/* 
           - Main Container
             - Video Background
