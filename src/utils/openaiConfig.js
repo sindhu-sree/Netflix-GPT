92d5f7e8912836ab46e247512bc06a5530cca4db
sindhu-sree <sindunarayana22@gmail.com>
2025-01-29 15:07:24 +0530

    Secured keys

--- src/utils/openaiConfig.js
+++ src/utils/openaiConfig.js
@@ -1,4 +1,5 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyCmzLcpNlsCMdqfw3guwPZJdle8Tg30yjA");
+
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GOOGLE_API_KEY);
 export default genAI
